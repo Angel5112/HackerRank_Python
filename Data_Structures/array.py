3d101d6c4f49fe0af8ead9999800c596a76268dd
Angel Eduardo <aepatino.360@gmail.com>
2023-03-12 20:06:26 -0400

    Add code of Arrays problem, send to tests.

---
+++ Data_Structures/array.py
@@ -0,0 +1,35 @@
+# Reverse an array of integers.
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def reverseArray(a):
+    # Write your code here
+    new_array = []
+
+    for i in range(len(a) - 1, -1, -1):
+        new_array.append(i)
+
+    return new_array
+
+
+
+if __name__ == '__main__':
+
+    arr_count = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    res = reverseArray(arr)
+
+    print(res)
